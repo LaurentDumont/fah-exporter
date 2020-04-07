6fbebd06b049339c57f821eb77bffd65c1d6adab
Laurent Dumont <laurentfdumont@gmail.com>
2020-04-07 00:48:16 -0400

    Add multiprocess

--- fah-exporter.py
+++ fah-exporter.py
@@ -6,6 +6,7 @@ import re
 import json
 import os
 import multiprocessing
+from functools import partial
 
 # {
 #    "id": "00",
@@ -27,6 +28,40 @@ import multiprocessing
 #    "reason": "",
 #    "idle": false
 # }
+
+
+def get_fah_output(info_type, target):
+
+    if info_type == "queues":
+        command = "queue-info"
+        regex = r'PyON 1 units((?:.*\r?\n?)*)---'
+
+    if info_type == "slots":
+        command = "slot-info"
+        regex = r'PyON 1 slots((?:.*\r?\n?)*)---'
+    fah_command = generate_fah_command(target, command)
+    command_result = subprocess.run([fah_command], shell=True,
+                                    capture_output=True, text=True, timeout=20)
+    raw_text = command_result.stdout
+    filtered_text = re.search(regex, raw_text)
+    pyon = filtered_text.groups()[0].replace("False", "false")
+    # To get this compatible with json - we need lowercase true and false
+    json_compatible = pyon.replace("True", "true")
+
+    fah_json = json.loads(json_compatible)
+    worker_json = {'worker_name': target}
+    for data in fah_json:
+        data.update(worker_json)
+
+    return fah_json
+
+
+def thread_fah_requests(targets, info_type):
+    process_pool = multiprocessing.Pool(multiprocessing.cpu_count())
+    partial_function = partial(get_fah_output, info_type)
+    json_data = process_pool.map(partial_function, targets)
+    process_pool.close()
+    return json_data
 
 
 def generate_fah_command(client, command):
@@ -64,49 +99,29 @@ class FahCollector(object):
             'fah_job_progress', 'FAH job percentage progress', 'gauge')
         fah_job_status = Metric(
             'fah_job_status', 'FAH job status', 'gauge')
-        for client in fah_clients:
-            command = "slot-info"
-            fah_command = generate_fah_command(client, command)
-            command_result = subprocess.run([fah_command], shell=True,
-                                            capture_output=True, text=True, timeout=5)
-            raw_text = command_result.stdout
-            filtered_text = re.search(
-                r'PyON 1 slots((?:.*\r?\n?)*)---', raw_text)
 
-            pyon = filtered_text.groups()[0].replace("False", "false")
-            # To get this compatible with json - we need lowercase true and false
-            json_compatible = pyon.replace("True", "true")
-            fah_slots_json = json.loads(json_compatible)
-
-            for slot in fah_slots_json:
+        info_type = "slots"
+        json_data = thread_fah_requests(fah_clients, info_type)
+        for data in json_data:
+            for slot in data:
                 fah_slot_metric.add_sample('fah_slot_info', value=convert_fah_status(slot['status']), labels={
                     'slot_id': str(slot['id']),
                     'description': str(slot['description']),
-                    'fah_worker': str(client),
+                    'fah_worker': str(slot['worker_name']),
                 })
-
-            command = "queue-info"
-            fah_command = generate_fah_command(client, command)
-            command_result = subprocess.run([fah_command], shell=True,
-                                            capture_output=True, text=True, timeout=5)
-            raw_text = command_result.stdout
-            filtered_text = re.search(
-                r'PyON 1 units((?:.*\r?\n?)*)---', raw_text)
-            pyon = filtered_text.groups()[0].replace("False", "false")
-            # To get this compatible with json - we need lowercase true and false
-            json_compatible = pyon.replace("True", "true")
-            fah_jobs_info = json.loads(json_compatible)
-
-            for job in fah_jobs_info:
+        info_type = "queues"
+        json_data = thread_fah_requests(fah_clients, info_type)
+        for data in json_data:
+            for job in data:
                 fah_job_status.add_sample('fah_job_status', value=convert_fah_status(job['state']), labels={
                     'project': str(job['project']),
-                    'fah_worker': str(client),
+                    'fah_worker': str(job['worker_name']),
                 })
                 fah_job_progress.add_sample('fah_job_progress', value=job['percentdone'].replace("%", ""), labels={
-                    'fah_worker': str(client),
+                    'fah_worker': str(job['worker_name']),
                 })
                 fah_job_credit.add_sample('fah_job_credit', value=job['creditestimate'], labels={
-                    'fah_worker': str(client),
+                    'fah_worker': str(job['worker_name']),
                 })
 
         yield fah_slot_metric
--- get-data.py
+++ get-data.py
@@ -1,6 +1,7 @@
 import subprocess
 import re
 import json
+import multiprocessing
 
 command = """
 nc 10.200.10.101 36330 <<EOF
@@ -10,13 +11,21 @@ EOF
 """
 
 
-command_result = subprocess.run([command], shell=True,
-                                capture_output=True, text=True, timeout=5)
-raw_text = command_result.stdout
-filtered_text = re.search(r'PyON 1 units((?:.*\r?\n?)*)---', raw_text)
+def get_fah_data(target):
+    command_result = subprocess.run([command], shell=True,
+                                    capture_output=True, text=True, timeout=15)
+    raw_text = command_result.stdout
+    filtered_text = re.search(r'PyON 1 units((?:.*\r?\n?)*)---', raw_text)
 
-pyon_data = filtered_text.groups()[0].replace("False", "false")
-json_compatible_data = pyon_data.replace("True", "true")
+    pyon_data = filtered_text.groups()[0].replace("False", "false")
+    json_compatible_data = pyon_data.replace("True", "true")
 
-json_data = json.loads(json_compatible_data)
-print(json.dumps(json_data, indent=4))
+    json_data = json.loads(json_compatible_data)
+    return json_data
+
+
+if __name__ == '__main__':
+    targets = ['10.200.10.101', '10.200.10.101']
+    p = multiprocessing.Pool(multiprocessing.cpu_count())
+    test = p.map(get_fah_data, targets)
+    print(test)
